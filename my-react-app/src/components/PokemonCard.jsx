ea9f893310f80062c49394813325d76a5c09d91c
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-03-26 11:33:55 +0100

    Add a Pokemon Card with Bulbasaur

---
+++ my-react-app/src/components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+function PokemonCard() {
+    return (
+      <figure>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="image de Bulbilzarre"
+        />
+        <figcaption>Bulbizarre, pas le meilleur des starters 🥺 </figcaption>
+      </figure>
+    );
+  }
+  export default PokemonCard;
